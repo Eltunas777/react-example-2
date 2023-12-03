e798e33b2b63d3a89f76f6aead5bb6f72fc0062b
Eltunas777 <cmujicacortes@gmail.com>
2023-12-03 12:26:09 -0300

    Yes 2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://eltunas777.github.io/react-task-example/",
+  base: "https://Eltunas777.github.io/react-example-2",
 })
